e433c4cc67f747ced33526a0a09b05a89341b0b3
odadki <93558662+odadki@users.noreply.github.com>
2023-06-28 10:09:37 -0700

    test

--- front-page.php
+++ front-page.php
@@ -1,11 +1,13 @@
 <?php 
-$image = get_field('test_image');
+
+$image = get_field('cb01_image');
+$body = get_field('cb01_body');
 ?>
 
 <section>
-	<h2 class="test"><?php echo esc_html( get_field('test_text') ); ?></h2>
+	<h2 class="test"><?php echo esc_html(get_field('cb01_title')); ?></h2>
 	<img src="<?php echo $image['url']; ?>" />
-	<?php echo esc_html(get_field('test_body')); ?>
+	<?php echo $body; ?>
 	<p>added p</p>
-	<p>added another p</p>
+	<!-- <p>added another p</p> -->
 </section>
